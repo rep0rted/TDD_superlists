b02f4c6d30200d32e5559b0dd03161af46fe3c2b
rep0rted <darby@bitchimight.be>
2016-09-02 20:53:22 -0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -18,16 +18,34 @@ class NewVisitorTest(unittest.TestCase):
 		# He notices the page title and header mention to-do
 		# lists
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		# He is invited to enter a  to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+
+		# He types "Buy pee cock feathers" into a text box (Forest's
+		# hobby is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
+
+		# When he hits enter, the page updates, and now the page lists
+		# "1: Buy pee cock feathers" as an item in a to-do list table 
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
+		# There is still a text box inviting him to add another item. He
+		# enters "Use pee cock feathers to make a fly" (He is very anal)
 		self.fail('Finish the test!')
-# He is invited to enter a  to-do item straight away
-#
-# He types "Buy pee cock feathers" into a text box (Forest's
-# hobby is tying fly-fishing lures)
-#
-# When he hits enter, the page updates, and now the page
-# lists "1: Buy pee cock feathers to make a fly" 
-# (Forest is very anal)
-#
+
 # The page updates again, and now shows both items on his
 # list
 #
