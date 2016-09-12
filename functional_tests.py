bdf936b9465221b85946c5997677da337d139493
rep0rted <darby@bitchimight.be>
2016-09-12 18:57:39 -0500

    Re-commit Ch4 'Front page html now gen from a template'

--- functional_tests.py
+++ functional_tests.py
@@ -4,49 +4,49 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+    self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+  def tearDown(self):
+    self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		# Forest has heard about a cool new online to-do app. He
-		# goes to check out its homepage
-		self.browser.get('http://localhost:8000')
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    # Forest has heard about a cool new online to-do app. He
+    # goes to check out its homepage
+    self.browser.get('http://localhost:8000')
 
-		# He notices the page title and header mention to-do
-		# lists
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+    # He notices the page title and header mention to-do
+    # lists
+    self.assertIn('To-Do', self.browser.title)
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
 
-		# He is invited to enter a to-do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-			inputbox.get_attribute('placeholder'),
-			'Enter a to-do item'
-		)
+    # He is invited to enter a to-do item straight away
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item'
+    )
 
-		# He types "Buy pee cock feathers" into a text box (Forest's
-		# hobby is tying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
+    # He types "Buy pee cock feathers" into a text box (Forest's
+    # hobby is tying fly-fishing lures)
+    inputbox.send_keys('Buy peacock feathers')
 
-		# When he hits enter, the page updates, and now the page lists
-		# "1: Buy pee cock feathers" as an item in a to-do list table 
-		inputbox.send_keys(Keys.ENTER)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows), 
-		"New to-do item did not appear in table"
-		)
-
-		# There is still a text box inviting him to add another item. He
-		# enters "Use pee cock feathers to make a fly" (He is very anal)
-		self.fail('Finish the test!')
+    # When he hits enter, the page updates, and now the page lists
+    # "1: Buy pee cock feathers" as an item in a to-do list table
+    inputbox.send_keys(Keys.ENTER)
+    
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(
+      any(row.text == '1:Buy peacock feathers' for row in rows),
+      "New to-do item did not appear in table"
+    )
+    
+    # There is still a text box inviting him to add another item. He
+    # enters "Use pee cock feathers to make a fly" (He is very anal)
+    self.fail('Finish the test!')
 
 # The page updates again, and now shows both items on his
 # list
@@ -59,4 +59,4 @@ class NewVisitorTest(unittest.TestCase):
 #
 # Satisfied, he goes back to sleep
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+  unittest.main(warnings='ignore')
