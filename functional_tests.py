4a0ce103d9d3005803a7e5ee166dfc0fe9faa286
rep0rted <darby@bitchimight.be>
2016-09-29 13:05:51 -0500

    Refactored FT

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
   def tearDown(self):
     self.browser.quit()
 
+  def check_for_row_in_list_table(self, row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn(row_text, [row.text for row in rows])
+
   def test_can_start_a_list_and_retrieve_it_later(self):
     # Forest has heard about a cool new online to-do app. He
     # goes to check out its homepage
@@ -36,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
     # When he hits enter, the page updates, and now the page lists
     # "1: Buy pee cock feathers" as an item in a to-do list table
     inputbox.send_keys(Keys.ENTER)
-    
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
     
     # There is still a text box inviting him to add another item. He
     # enters "Use pee cock feathers to make a fly" (He is very anal)
@@ -49,14 +51,9 @@ class NewVisitorTest(unittest.TestCase):
 
     # The page updates again, and now shows both items on his
     # list
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-    self.assertIn(
-      '2: Use peacock feathers to make a fly' ,
-      [row.text for row in rows]
-    )
-    
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
+    self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
     # Forest wonders whether the site will remember his list.
     # Then he sees that the site has generated a unique URL for
     # him -- there is some explanatory text to that effect
